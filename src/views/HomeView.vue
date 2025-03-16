19d8012ccbed1adfe4d9925ada60e83abc8e3a0b
AbsintheSung <h90243768@gmail.com>
2025-03-16 23:50:42 +0800

    update:將首頁卡內容改成content.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import LoGo from '@/components/LoGo.vue'
 import NavBar from '@/components/NavBar.vue'
+import { characteristic as charData } from '@/content/characteristicText'
 const isOpen = ref(false)
 const isLoading = ref(true)
 const bgImgRef = ref(null)
 const toggleMenu = () => {
   isOpen.value = !isOpen.value
 }
+const getCharData = computed(() => {
+  return charData.map((item) => ({
+    title: item.title.split(''),
+    content: item.content,
+  }))
+})
 
 const imgLoadingFinish = (url) => {
   const img = new Image()
@@ -73,32 +80,15 @@ onMounted(() => {
           </div>
           <div class="bg-[#DCCCBC] mt-auto py-12 px-9 lg:py-14 lg:px-12">
             <ul class="flex flex-col gap-y-12 md:gap-x-8 lg:flex-row lg:gap-x-18">
-              <li class="flex gap-x-9">
+              <li class="flex gap-x-9" v-for="charItem in getCharData" :key="charItem.title">
                 <h4 class="text-4xl font-bold">
-                  <span>外</span>
-                  <span class="circles">觀</span>
+                  <template v-for="(titleItem, index) in charItem.title" :key="titleItem + index">
+                    <span :class="{ circles: index === charItem.title.length - 1 }">
+                      {{ titleItem }}
+                    </span>
+                  </template>
                 </h4>
-                <p>
-                  白頭鵯體長約17到22公分，額至頭頂純黑色而富有光澤，兩眼上方至後枕白色，形成一白色枕環。耳羽後部有一白斑，此白環與白斑在黑色的頭部均極為醒目，老鳥的枕羽(後頭部)更潔白，所以又叫「白頭翁」。
-                </p>
-              </li>
-              <li class="flex gap-x-9">
-                <h4 class="text-4xl font-bold">
-                  <span>棲</span>
-                  <span class="circles">地</span>
-                </h4>
-                <p>
-                  白頭翁和麻雀、綠繡眼合稱「城市三寶」，常成群出現在平原區灌木叢、丘陵樹林地帶，以及校園、公園、庭院、行道中的各種高高的電線與樹上。
-                </p>
-              </li>
-              <li class="flex gap-x-9">
-                <h4 class="text-4xl font-bold">
-                  <span>食</span>
-                  <span class="circles">性</span>
-                </h4>
-                <p>
-                  以果樹的漿果和種子為主食，並時常飛入果園偷吃果實，還會吃嫩葉嫩芽，尤其是胡蝶蘭的嫩葉嫩芽葉，偶爾啄食昆蟲。
-                </p>
+                <p>{{ charItem.content }}</p>
               </li>
             </ul>
           </div>
